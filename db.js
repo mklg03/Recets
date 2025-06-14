3328683cff1a2734bbe4a28eea1c189bae8628e8
mklg03 <mykelgarca@gmail.com>
2025-06-14 00:20:13 -0400

    commit2

--- db.js
+++ db.js
@@ -11,12 +11,25 @@ const pool = new Pool({
 module.exports = pool;
  */
 
-const { Pool } = require("pg");
+/* const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: "postgresql://postgres:s10045718@db.wgqtwucvszplvqvpdyvc.supabase.co:5432/postgres",
   ssl: {
     rejectUnauthorized: false, // requerido para Supabase
+  },
+});
+
+module.exports = pool;
+ */
+
+require("dotenv").config();
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
   },
 });
 
